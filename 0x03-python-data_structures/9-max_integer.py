6e91f25df0823f33fa7c745d9497c1947de4b5f6
Anas90-Top <anasmahmoud688@gmail.com>
2023-01-08 16:07:18 -0800

    Goo

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,26 @@
 #!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to find the biggest integer of a list
+#
+# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
+# email igbinijesusamuel@gmail.com
+# -----------------------------------------------------------
+
+
 def max_integer(my_list=[]):
-    if not my_list:
+    """
+    Args:
+        my_list: a list
+    Returns:
+        the biggest integer in list or none if list is empty
+    """
+
+    if len(my_list) == 0:
         return None
-    biggest = 0
-    for elem in my_list:
-        if elem > biggest:
-            biggest = elem
-    return biggest
+    else:
+        biggest_int = my_list[0]
+        for index in range(len(my_list)):
+            if my_list[index] > biggest_int:
+                biggest_int = my_list[index]
+    return biggest_int
